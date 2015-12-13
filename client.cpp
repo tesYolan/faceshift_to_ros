650712e62774dd14ef44964956262b6053e670c1
tesyolan <tesfa1993@yaoo.com>
2015-12-13 12:48:22 +0300

    Update to output the results of the Blendshape to the tracking results.

--- client.cpp
+++ client.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
+#include <signal.h>
 #include "fsbinarystream.h"
 using boost::asio::ip::tcp;
 
@@ -16,6 +17,7 @@ int main(int argc, char* argv[])
     }
     fs::fsBinaryStream parserIn, parserOut;
     fs::fsMsgPtr msg;
+
     // Any program that uses asio need to have at least one io_service object
     boost::asio::io_service io_service;
 
@@ -39,7 +41,9 @@ int main(int argc, char* argv[])
     // The boost::asio::connect() function does this for us automatically.
     tcp::socket socket(io_service);
     boost::asio::connect(socket, endpoint_iterator);
-
+    bool firstrun = true;
+    size_t len;
+    std::vector<std::string> blendshape_names;
     // The connection is open. All we need to do now is read the response from the daytime service.
     for (;;)
     {
@@ -47,71 +51,75 @@ int main(int argc, char* argv[])
       boost::array<char, 128> buf;
       boost::system::error_code error;
 
-      // The boost::asio::buffer() function automatically determines
-      // the size of the array to help prevent buffer overruns.
-      size_t len = socket.read_some(boost::asio::buffer(buf), error);
+      //Now this is an overkill but we need to get the blendshape names in every packet
+      if (firstrun)
+      {
+        fs::fsMsgSendBlendshapeNames bln;
+        std::string datatosend;
+        parserOut.encode_message(datatosend, bln);
+        socket.send(boost::asio::buffer(datatosend));
+        firstrun = false;
+      }
 
-      // When the server closes the connection,
-      // the ip::tcp::socket::read_some() function will exit with the boost::asio::error::eof error,
-      // which is how we know to exit the loop.
+      len = socket.read_some(boost::asio::buffer(buf), error);
       if (error == boost::asio::error::eof)
         break; // Connection closed cleanly by peer.
       else if (error)
         throw boost::system::system_error(error); // Some other error.
-
-      // std::cout.write(buf.data(), len);
-      //Now the program would have exited if it had reached here without data
       parserIn.received(len, buf.data());//As it taked (long int sz, const *data)
-
       while (msg = parserIn.get_message())
       {
+        // printf("The output of the calss is: %d\n", msg.get()->id() );
         if (dynamic_cast<fs::fsMsgTrackingState*>(msg.get()))
         {
 
           fs::fsMsgTrackingState *ts = dynamic_cast<fs::fsMsgTrackingState*>(msg.get());
           const fs::fsTrackingData & data = ts->tracking_data();
 
-          // for (std::vector<std::string>::iterator i = blend.begin(); i != blend.end(); ++i)
-          // {
-          //   printf("%s\n", (*i).c_str());
-          // }
-
-          //Now this is enough for testing but the name of the blendshapes must be found in the system.
-          // std::vector<std::string> blender_shapes = bn->blendshape_names();
-
-
+          printf ("Time: %f \n", data.m_timestamp);
+          printf ("Tracking Results: %s", data.m_trackingSuccessful ? "true" : "false");
           printf ("head translation: %f %f %f\n", data.m_headTranslation.x, data.m_headTranslation.y, data.m_headTranslation.z);
           printf ("head rotation: %f %f %f %f \n", data.m_headRotation.x, data.m_headRotation.y, data.m_headRotation.z, data.m_headRotation.w);
           printf ("Eye Gaze Left Pitch: %f\n", data.m_eyeGazeLeftPitch);
           printf ("Eye Gaze Left Pitch: %f\n", data.m_eyeGazeLeftYaw);
           printf ("Eye Gaze Left Pitch: %f\n", data.m_eyeGazeRightPitch);
           printf ("Eye Gaze Left Pitch: %f\n", data.m_eyeGazeRightYaw);
-          int counter = 0;
+
           std::vector<float> blend_shape = data.m_coeffs;
-          for (std::vector<float>::iterator i = blend_shape.begin(); i != blend_shape.end(); ++i)
+
+          //Before pulishing it check if they are the same;
+          int counter = 0;
+
+          if (blendshape_names.size() == blend_shape.size())
           {
-            printf("Blendshape %d %f\n", counter, *i);
-            counter++;
+            for (std::vector<float>::iterator i = blend_shape.begin(); i != blend_shape.end(); ++i)
+            {
+              printf("Blendshape %s : %f\n", blendshape_names[counter].c_str(),  *i);
+              counter++;
+            }
           }
+          else {
+            firstrun = true; //Cause the number of blendshapes and the rigging data have now changed in the system.
+            printf("There is a mismatch querying for blendshape names. \n");
+          }
+
         }
+
         if (dynamic_cast<fs::fsMsgBlendshapeNames*>(msg.get()))
         {
-          fs::fsMsgBlendshapeNames *bn = dynamic_cast<fs::fsMsgBlendshapeNames*>(msg.get());
+          fs::fsMsgBlendshapeNames *bs = dynamic_cast<fs::fsMsgBlendshapeNames*>(msg.get());
 
-          std::vector<std::string> blend = bn->blendshape_names();
-          int counter = 0;
-          for (std::vector<std::string>::iterator i = blend.begin(); i != blend.end(); ++i)
+          blendshape_names = bs->blendshape_names();
+
+          for (std::vector<std::string>::iterator i = blendshape_names.begin();
+               i != blendshape_names.end(); ++i)
           {
-            printf("Blendshape %d %s\n", counter, (*i).c_str());
-            counter++;
+            printf("Blendshape name %s\n", (*i).c_str() );
           }
 
+        }
 
-        }
-        else{
-          //Not getting data
-          printf("Blendshapes not found\n" );
-        }
+
       }
       if (!parserIn.valid()) {
         printf("parser in invalid state\n");
